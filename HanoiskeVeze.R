05baca67d4128a99b2dbe976e89dee5566b88eed
Pavel Hornicek <195714@tul.cz>
2021-10-07 17:15:29 +0200

    Verze 2 s par opravami

--- HanoiskeVeze.R
+++ HanoiskeVeze.R
@@ -2,11 +2,11 @@
 
 Hanoi <- function(n,zKoliku,naKolik) {
   if (n==1){
-    cat('Presun dis y koliku',zKoliku,'na kolik',naKolik,'\n', sep=' ')
+    cat('Presun disk z koliku',zKoliku,'na kolik',naKolik,'\n', sep=' ')
   } else {
         volnyKolik <- 6-zKoliku-naKolik
         Hanoi(n-1,zKoliku,volnyKolik)
-        cat('Presun disk z koliku',zKoliku,'na kolik',naKolik, '\n' sep= ' ')
+        cat('Presun disk z koliku',zKoliku,'na kolik',naKolik,'\n', sep=' ')
         Hanoi(n-1,volnyKolik,naKolik)
       }
 }
