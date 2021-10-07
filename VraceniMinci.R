7391542f9902b0550fd07494cccc2923854a01f6
Pavel Hornicek <195714@tul.cz>
2021-10-07 16:21:58 +0200

    Verze1

--- VraceniMinci.R
+++ VraceniMinci.R
@@ -1,7 +1,49 @@
-% pokus o algoritmus vraceni minci
+# pokus o algoritmus vraceni minci
 
-VraceniMinci(M);
+VraceniMinci<-function(M) {
+  
+  Mince = c(50,20,10,5,2,1);
+  Pocet = 0;
+  
+  while (M>0) {if (M>= Mince[1]){
+    M = M - Mince[1]
+    Pocet = Pocet + 1 
+  }
+    else{(M>= Mince[2]){
+      M = M - Mince[2]
+      Pocet = Pocet + 1
+    }
+      else(M>= Mince[3]){
+        M = M - Mince[3]
+        Pocet = Pocet + 1
+      } 
+        else(M>= Mince[4]){
+          M = M - Mince[4]
+          Pocet = Pocet + 1
+        }
+          else(M>= Mince[5]){
+            M = M - Mince[5]
+            Pocet = Pocet + 1
+          }
+            else(M>= Mince[6]){
+              M = M - Mince[6]
+              Pocet = Pocet + 1
+            }
+    }
+    
+    
+  }
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    }
 
-c = {50, 20, 10, 5, 2 ,1}
-
-while (M>0)
